43dcbdf22f6fd30b6385f87f0f1bed500fb8ef9e
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-27 21:02:33 -0300

    Alterando para composição em vez de herança

---
+++ src/main/java/jackson/rocha/locadora/SeguroSantander.java
@@ -0,0 +1,9 @@
+package jackson.rocha.locadora;
+
+public class SeguroSantander implements Seguro {
+
+    @Override
+    public double calcularPremio(int horasUtilizadas, double valorContrato) {
+        return (horasUtilizadas * 0.50) + (valorContrato * 0.05);
+    }
+}
